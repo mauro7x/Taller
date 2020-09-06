194e2dbc7281827dc6896bda408b14e2548b81a6
Mauro7x <mparafati@fi.uba.ar>
2020-09-05 22:15:37 -0300

    esqueletos

---
+++ Examen Final/Esqueletos/client.c
@@ -0,0 +1,30 @@
+// Includes para sockets POSIX
+#define _POSIX_C_SOURCE 200112L
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// Includes propios de C
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    // Chequeo de parámetros
+    // ...
+
+    // getaddrinfo
+    // ...
+
+    // Connect
+    // ...
+
+    // Ejercicio particular
+    // ...
+
+    // Liberar recursos
+    // ...
+
+    return 0;
+}
---
+++ Examen Final/Esqueletos/server.c
@@ -0,0 +1,39 @@
+// Includes para sockets POSIX
+#define _POSIX_C_SOURCE 200112L
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// Includes propios de C
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    // Chequeo de parámetros
+    // ...
+
+    // getaddrinfo
+    // ...
+
+    // Socket accepter
+    // ...
+
+    // Bind
+    // ...
+
+    // Listen
+    // ...
+
+    // Accept
+    // ...
+
+    // Ejercicio particular
+    // ...
+
+    // Liberar recursos
+    // ...
+
+    return 0;
+}
